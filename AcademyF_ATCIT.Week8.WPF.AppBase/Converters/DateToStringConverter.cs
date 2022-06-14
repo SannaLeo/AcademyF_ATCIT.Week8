4fed39586f4bedb08f4f8157dfa5a8538a28a3e5
SannaLeo <sannaleonardo1@gmail.com>
2022-06-14 09:11:52 +0200

    Add project files.

---
+++ AcademyF_ATCIT.Week8.WPF.AppBase/Converters/DateToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AcademyF_ATCIT.Week8.WPF.AppBase.Converters
+{
+    public class DateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var date = (DateTime)value;
+                return date.ToShortDateString();
+            }
+            else return "campo vuoto";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
